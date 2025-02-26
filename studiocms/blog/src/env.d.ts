5afac2de2a906d5ba1d7b2a08b925701d2c7607e
Adam Matthiesen <amatthiesen@outlook.com>
2025-02-26 05:06:04 -0800

    Replace favicon.svg with new logo design and remove unused env.d.ts file

--- studiocms/blog/src/env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-/// <reference types="../.astro/@studiocms/blog.d.ts" />
-/// <reference types="../.astro/studiocms-blog.d.ts" />
