546f9a601d0dd1645efc4650bed3f8076b2583ec
Adam Matthiesen <amatthiesen@outlook.com>
2025-02-26 06:00:04 -0800

    Remove devApps import and downgrade StudioCMS dependencies to beta.7

--- studiocms/headless/astro.config.mjs
+++ studiocms/headless/astro.config.mjs
@@ -1,6 +1,6 @@
 import db from '@astrojs/db';
 import node from '@astrojs/node';
-import devApps from '@studiocms/devapps';
+// import devApps from '@studiocms/devapps';
 import { defineConfig } from 'astro/config';
 import studioCMS from 'studiocms';
 
