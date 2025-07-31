e4030f83728450deff4bef28b7a27b62ae4bba65
Adam Matthiesen <amatthiesen@outlook.com>
2025-07-30 21:36:31 -0700

    update dependencies and remove unused assets; add markdown and HTML plugins

--- studiocms/blog/studiocms.config.mjs
+++ studiocms/blog/studiocms.config.mjs
@@ -1,7 +1,8 @@
-import blog from '@studiocms/blog';
 import { defineStudioCMSConfig } from 'studiocms/config';
+import blog from '@studiocms/blog';
+import md from '@studiocms/md';
 
 export default defineStudioCMSConfig({
 	dbStartPage: true,
-	plugins: [blog()],
+	plugins: [md(), blog()],
 });
--- studiocms/headless/studiocms.config.mjs
+++ studiocms/headless/studiocms.config.mjs
@@ -1,5 +1,8 @@
 import { defineStudioCMSConfig } from 'studiocms/config';
+import md from '@studiocms/md';
+import html from '@studiocms/html';
 
 export default defineStudioCMSConfig({
 	dbStartPage: true,
+	plugins: [md(), html()],
 });
